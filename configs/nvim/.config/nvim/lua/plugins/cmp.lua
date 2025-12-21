f5be6cee8b10250eaad4415946c19ab8081d5b47
lausan3 <lauanthony4455@gmail.com>
2025-12-21 00:11:27 -0500

    feat(nvim): add lsps for commonly used languages

--- configs/nvim/.config/nvim/lua/plugins/cmp.lua
+++ configs/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,9 +1,23 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-cmdline",
+    },
     -- @param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
+
+      cmp.setup(opts)
+
+      -- Set up cmdline completion for Noice
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+          { name = "cmdline" },
+        },
+      })
 
       -- This merges your new mappings into the existing LazyVim mappings
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
