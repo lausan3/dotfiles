b05a59436fe7257f731cfe9b039bdc85cf5b2620
lausan3 <lauanthony4455@gmail.com>
2025-12-20 20:37:19 -0500

    refactor(nvim): remove old NvChad configs

--- configs/nvim/.config/nvim/lua/old/custom/chadrc.lua
+++
@@ -1,6 +0,0 @@
----@type ChadrcConfig
-local M = {}
-
-M.ui = { theme = 'tokyonight' }
-
-return M
--- configs/nvim/.config/nvim/lua/plugins/options/nvim-tree.lua
+++
@@ -1,4 +0,0 @@
-return {
-  filters = { dotfiles = false },
-  display_stat = { country = false },
-}
