57a00b84b00cc8e5a1c80cdda0a1e15426d40f86
lausan3 <lauanthony4455@gmail.com>
2025-12-21 01:47:48 -0500

    fix(nvim): fix leader space key not being configured correctly

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -1,4 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.g.root_spec = { "cwd" }
--- configs/nvim/.config/nvim/lua/config/lazy.lua
+++ configs/nvim/.config/nvim/lua/config/lazy.lua
@@ -1,3 +1,6 @@
+vim.g.root_spec = { "cwd" }
+vim.g.mapleader = "<Space>"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
