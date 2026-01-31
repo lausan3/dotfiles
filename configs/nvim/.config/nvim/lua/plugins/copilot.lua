f68902b795d37b688780f915fec44e23ed1240a9
lausan3 <lauanthony4455@gmail.com>
2026-01-30 20:03:15 -0500

    feat(nvim): add next edit suggestion functionality to copilot plugin

--- configs/nvim/.config/nvim/lua/plugins/copilot.lua
+++ configs/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,12 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v22.22.0/bin/node",
-  },
+  -- "zbirenbaum/copilot.lua",
+  -- dependencies = {
+  --   "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
+  -- },
+  -- cmd = "Copilot",
+  -- event = "InsertEnter",
+  -- opts = {
+  --   copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v22.22.0/bin/node",
+  -- },
+  "github/copilot.vim",
 }
